7e66da71e7139481719c6733363642ed0158afec
pradanaadn <pradanaadn21@gmail.com>
2024-11-08 19:49:35 +0800

    chore: add env variable

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+from yaml import load, SafeLoader
+from loguru import logger
+
+try:
+    with open("secrets.yaml", "r") as file:
+        credential = load(file, Loader=SafeLoader)
+
+    GEMINI_API_KEY = credential["llm_key"]["gemini"]
+
+except Exception as e:
+    logger.exception(e)
+
+
