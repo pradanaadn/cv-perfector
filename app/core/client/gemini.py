b86a65eb1d8bf7186d31a90c62e7f5eb86f471a7
pradanaadn <pradanaadn21@gmail.com>
2024-11-09 17:25:43 +0800

    feat: initialize gemini

--- app/core/client/gemini.py
+++ app/core/client/gemini.py
@@ -1,7 +1,27 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from pydantic import BaseModel
+from app.schemas.llm import GeminiConfig
+from app.core.config import GEMINI_API_KEY
+
 
 class ChatGemini(BaseModel):
-    def __init__(self, model:str):
+    def __init__(
+        self,
+        model: str = "gemini-1.5-flash",
+        config: GeminiConfig = GeminiConfig(),
+        api_key: str = GEMINI_API_KEY,
+    ):
         self.model = model
-        self
+        self.config = config
+        self.api_key = api_key
+    def _init_llm(self):
+        llm = ChatGoogleGenerativeAI(
+            model=self.model,
+            temperature=self.config.temperature,
+            top_k=self.config.top_k,
+            top_p=self.config.top_p,
+            max_tokens=self.config.max_token,
+            response_mime_type=self.config.response_mime_type,
+            gemini_api_key = self.api_key
+        )
+        return llm
--- app/schemas/llm.py
+++ app/schemas/llm.py
@@ -5,6 +5,36 @@ from typing import Union, Literal
 
 @dataclass
 class GeminiConfig:
+    """Configuration parameters for generating responses from a Gemini model.
+
+    Attributes:
+        temperature (Union[int, float]): Controls the randomness of the generated text.  Higher values (closer to 2)
+            result in more random output, while lower values (closer to 0) make the output more deterministic. Must be
+            between 0 and 2 inclusive. Defaults to 1.
+        top_p (Union[int, float]): Controls the diversity of the generated text using nucleus sampling. At each step,
+            only tokens whose cumulative probability exceeds `top_p` are considered.  Must be between 0 and 1 inclusive.
+            Defaults to 0.95.
+        top_k (int): Controls the diversity of the generated text using top-k sampling. At each step, only the `top_k` most
+            likely tokens are considered. Must be greater than or equal to 1. Defaults to 40.
+        max_token (int): The maximum number of tokens to generate in the response. Must be between 0 and 10,000
+            inclusive. Defaults to 5,000.
+        response_mime_type (Literal["text/plain", "application/json"]): The MIME type of the response.  Can be either
+            "text/plain" for plain text or "application/json" for JSON. Defaults to "application/json".
+    
+    Instantiation:
+     .. code-block:: python
+        llm_config = GeminiConfig(
+        temperature = 0.5, 
+        top_p = 0.98,
+        top_k = 20, 
+        max_token = 1000,
+        response_mime_type = "text/plain" )
+
+    or you can just use the default value,
+     .. code-block:: python
+        llm_config = GeminiConfig()
+    """
+
     temperature: Union[int, float] = Field(ge=0, le=2, default=1)
     top_p: Union[int, float] = Field(ge=0, le=1, default=0.95)
     top_k: int = Field(ge=1, default=40)
