fe85a6d183b9cf73e9af158817fefed89bfa5366
pradanaadn <pradanaadn21@gmail.com>
2025-01-12 22:15:19 +0800

    feat: add pdf parser

--- app/infrastructure/pdf/pdf_parser.py
+++ app/infrastructure/pdf/pdf_parser.py
@@ -1,4 +1,13 @@
-
+import pymupdf4llm 
+from typing import Union
+from langchain.llms.base import BaseLanguageModel, BaseLLM
 class PDFParser:
-    def __init__(self, file_path:str):
+    def __init__(self, file_path:str, llm:Union[BaseLLM, BaseLanguageModel]):
         self.file_path = file_path
+        self.llm = llm
+        
+    def extract_text(self)->str:
+        md_text = pymupdf4llm.to_markdown(self.file_path)
+        return md_text
+    def extract_to_object(self):
+        pass
